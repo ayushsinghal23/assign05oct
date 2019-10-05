7d15f635f15148efb65dd9f88eb339cc27561142
ayushsinghal23 <ayush.singhal_cs18@gla.ac.in>
2019-10-05 16:22:47 +0530

    init commit question 1 completed

---
+++ src/Main1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main1 {
+    private String input;
+
+    public Main1(String input) {
+        this.input = input;
+
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void modification() {
+        System.out.print("Index: ");
+        for (int i = 0; i < getInput().length(); i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.print("\n");
+        System.out.print("Chars: ");
+        for (int i = 0; i < getInput().length(); i++) {
+            System.out.print(getInput().charAt(i) + "\t");
+
+
+        }
+    }
+}
+
+class modify {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        //  String input = scanner.nextLine().trim();
+        Main1 obj1 = new Main1("Hello");
+        obj1.modification();
+    }
+}
